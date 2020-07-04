46c64a08b22dbec2657afdb90cdcca6c137d1542
paznogard <paznogard@gmail.com>
2020-07-03 20:49:20 -0500

    Modificacion e implementacion de rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,26 @@ import { NgModule } from '@angular/core';
 // Routes
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PageModule } from './pages/pages.module';
+
+
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { SharedComponent } from './shared/shared.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    BreadcrumbsComponent,
-    SharedComponent,
-    SidebarComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
